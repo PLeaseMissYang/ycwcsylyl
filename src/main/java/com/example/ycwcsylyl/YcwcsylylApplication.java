7a7a55c77d2e148e45185db29524c970bcc13658
pleasemissyang <863641231@qq.com>
2019-12-16 13:17:17 +0800

    first commit

---
+++ src/main/java/com/example/ycwcsylyl/YcwcsylylApplication.java
@@ -0,0 +1,13 @@
+package com.example.ycwcsylyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YcwcsylylApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YcwcsylylApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ycwcsylyl/YcwcsylylApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ycwcsylyl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YcwcsylylApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
